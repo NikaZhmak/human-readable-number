f9f3f00f65a6806d3f053225d81af3e25d920554
Nika Zhmachinskaya <95099394+NikaZhmak@users.noreply.github.com>
2022-06-28 00:33:11 +0300

    feat:add return

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 module.exports = function toReadable (number) {
- let toTwenthy = ['','one','two','three','four','five','six','seven','eight','nine','ten','eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen',
+ let toTwenthy = ['','one','two','three','four','five','six','seven','eight','nine','','eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen',
   'sixteen', 'seventeen',  'eighteen', 'nineteen',]
- 
+ let readable =""
   let tenth = ['','',
              'twenty',
              'thirty',
@@ -14,30 +14,41 @@ module.exports = function toReadable (number) {
  
  function numbers(number){
        if(number==0){
-     return "zero"
+        let readable =+"zero"
+     return readable
  
  } 
  else if(number<20){
- 
-             return toTwenthy[number]
+
+   let readable =+toTwenthy[number]
+   return readable
+              
           }
  
  
  else if(number<100){
              if(number%10==0){
-                return tenth[number/10]
+               let readable =+tenth[number/10]
+               return readable
+                 
              }
  
        else if(number%10!==0){
              if(number<100){
                    let integerhPart = tenth[Math.trunc(number/10)]
                    let tenthPart = toTwenthy[Math.ceil(((number/10)-Math.trunc(number/10))*10)]
-                   let numb = ""
-                   return numb +integerhPart+" "+tenthPart}   
+
+                   let readable =+ +integerhPart+" "+tenthPart
+               return readable
+                  //  let numb = ""
+                  //  return numb +integerhPart+" "+tenthPart
+               }   
  
       } 
              else if(number===100){
-                      return "one hundred"
+               let readable =+ "one hundred"
+               return readable
+                       
                    }
        }
 
@@ -48,12 +59,17 @@ module.exports = function toReadable (number) {
                    let firstDigit = toTwenthy[+numberToString[0]];
                    let secDigit =tenth[+numberToString[1]] ;
                    let therdDigit =toTwenthy[+numberToString[2]] ;
-                   return firstDigit +" " + "hundred" +" "+ secDigit+ " "+ therdDigit 
+                   let readable =+  'firstDigit +" " + "hundred" +" "+ secDigit+ " "+ therdDigit' 
+               return readable
+
+                  //  return firstDigit +" " + "hundred" +" "+ secDigit+ " "+ therdDigit 
              }   
                  
- else if(number===1000){
-    return "one thousand"
+ else if(number>=1000){
+   let readable =+ "one thousand" 
+               return readable
+   //  return "one thousand"
  }
+ return readable
  }
-    return
 }
